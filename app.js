d5e6c4850a7a489b5d6974a45b5141cf12bd7888
job kilonzo <32642682+jobkilonzo@users.noreply.github.com>
2025-06-04 04:13:35 +0300

    Created error middleware

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cookierParser())
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
